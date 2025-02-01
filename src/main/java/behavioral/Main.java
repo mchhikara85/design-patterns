eb6fdfa21ca2a403f1aaa967147cfed07104aaa9
Manjeet Chhikara <mchhikara@Manjeets-MBP.attlocal.net>
2025-02-01 15:19:36 -0500

    Iterator

--- src/main/java/behavioral/Main.java
+++ src/main/java/behavioral/Main.java
@@ -1,12 +1,15 @@
 package main.java.behavioral;
 
-import main.java.behavioral.observer.IObservable;
-import main.java.behavioral.observer.IObserver;
-import main.java.behavioral.observer.Product;
-import main.java.behavioral.observer.User;
+import main.java.behavioral.iterator.Aggregator;
+import main.java.behavioral.iterator.Book;
+import main.java.behavioral.iterator.Iterator;
+import main.java.behavioral.iterator.Library;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         /*List<HumanInitial> humans = new ArrayList<>();
         humans.add(new AdultInitial());
         humans.add(new ChildInitial());
@@ -21,13 +24,23 @@ public class Main {
             human.play();
         }*/
 
-        IObservable product = new Product();
+/*        IObservable product = new Product();
         IObserver user1  = new User(product);
         IObserver user2  = new User(product);
 
         product.registerObserver(user1);
         product.registerObserver(user2);
 
-        product.addProduct(5);
+        product.addProduct(5);*/
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Live in Pisgan", "Writer 1"));
+        books.add(new Book("Happy Bunny", "Writer 2"));
+        Aggregator aggregator = new Library(books);
+        Iterator<Book> iterator = aggregator.createIterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            System.out.println(book);
+        }
     }
 }
--- src/main/java/creational/Main.java
+++ src/main/java/creational/Main.java
@@ -1,11 +1,29 @@
 package main.java.creational;
 
+import main.java.creational.abstractfactory.FactoryProducer;
+import main.java.creational.abstractfactory.Vehicle;
+import main.java.creational.abstractfactory.VehicleFactory;
 import main.java.creational.builder.Student;
 import main.java.creational.builder.StudentBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
+        VehicleFactory luxuryVehicleFactory = FactoryProducer.getVehicleFactory(true);
+        VehicleFactory ordinaryVehicleFactory = FactoryProducer.getVehicleFactory(false);
+
+        List<Vehicle> vehicles = new ArrayList<>();
+        vehicles.add(ordinaryVehicleFactory.createVehicle("Maruti"));
+        vehicles.add(luxuryVehicleFactory.createVehicle("Mercedes"));
+        vehicles.add(luxuryVehicleFactory.createVehicle("BMW"));
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.details();
+        }
+
         Student student = new StudentBuilder()
                 .name("Abhimanyu")
                 .age(8)
